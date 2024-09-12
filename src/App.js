0f9fe0b900b8a11ede33e4bf5f0d85d2718cd21b
HKSP Digital <hkspdigital@gmail.com>
2024-09-12 14:19:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hasil Karya Inventory!</h1>
+        <p>untuk PO & manajemen inventory General Affairs.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
